9cf0ad51132141b20cb2f43f249d23c623a0c599
Михаил А. Демин <demin@isate.kz>
2020-11-25 15:04:29 +0600

    modify indent rule

--- packages/eslint-config-mide-base/.eslintrc.js
+++ packages/eslint-config-mide-base/.eslintrc.js
@@ -4,7 +4,26 @@ module.exports = {
     ],
     rules: {
         'arrow-body-style': 'off',
-        'indent': ['error', 4],
+        'indent': ['error', 4, {
+            ignoredNodes: [
+                'JSXElement',
+                'JSXElement > *',
+                'JSXAttribute',
+                'JSXIdentifier',
+                'JSXNamespacedName',
+                'JSXMemberExpression',
+                'JSXSpreadAttribute',
+                'JSXExpressionContainer',
+                'JSXOpeningElement',
+                'JSXClosingElement',
+                'JSXFragment',
+                'JSXOpeningFragment',
+                'JSXClosingFragment',
+                'JSXText',
+                'JSXEmptyExpression',
+                'JSXSpreadChild',
+            ]
+        }],
         'linebreak-style': 'off',
         'max-len': 'off',
         'no-console': ['error', { 'allow': ['warn', 'error'] }],
